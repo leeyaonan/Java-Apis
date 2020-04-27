57d2e1faf4b418dc06c4fe3211a82b0f33e0f942
leeyaonan <leeyaonan@outlook.com>
2020-04-27 11:52:15 +0800

    搭建SpringBoot项目框架

---
+++ src/main/java/com/leeyaonan/apis/JavaApisApplication.java
@@ -0,0 +1,13 @@
+package com.leeyaonan.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leeyaonan/apis/JavaApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeyaonan.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
